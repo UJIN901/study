1381dbff45aaad436b4b30ea5511d4c34be02cc1
UJIN901 <57745105+UJIN901@users.noreply.github.com>
2021-05-23 20:30:35 +0900

    baekjoon 10818

---
+++ baekjoon/10818.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+	vector<int> v(N);
+	for (int i = 0; i < N; i++) {
+		cin >> v[i];
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[0] << " " << v[N - 1];
+}
